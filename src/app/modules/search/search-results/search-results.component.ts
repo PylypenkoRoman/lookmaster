5a147b7eebc07efd52454f52fb365298d936ff83
Roman <Roman@romans-mbp.lv.remit.se>
2017-10-02 20:53:29 +0300

    add real-time search and add active button for pagination

--- src/app/modules/search/search-results/search-results.component.ts
+++ src/app/modules/search/search-results/search-results.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SearchService } from '../search.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'lm-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.styl']
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent {
+  results: Object;
+  searchTerm$ = new Subject<string>();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(private searchSVC: SearchService) {
+    this.searchSVC.realTimeSearch(this.searchTerm$)
+    .subscribe(results => {
+      this.results = results.results;
+    });
+   }
 }
+
+
--- src/app/modules/search/search.service.ts
+++ src/app/modules/search/search.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
 import * as firebase from 'firebase';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
 import { CategoryService } from "app/modules/category/category.service";
 import { SearchResult } from "app/modules/search/search/search-result.interface";
 import { Category } from "app/modules/category/category";
@@ -11,8 +14,10 @@ import { Category } from "app/modules/category/category";
 @Injectable()
 export class SearchService {
   categoryList = [];
+  baseUrl: string = 'https://api.cdnjs.com/libraries';
+  queryUrl: string = '?search=';
   
-  constructor(private categorySVC: CategoryService) {
+  constructor(private categorySVC: CategoryService, private http: Http) {
     this.categoryList = this.categorySVC.categoryList;
     }
 
@@ -24,4 +29,18 @@ export class SearchService {
     let categoryResultPage = categoryResult.slice((page - 1) * limit, page * limit);
     return Observable.of({total: categoryResult.length, items:categoryResultPage}).delay(100);
   }
+
+
+  realTimeSearch(terms: Observable<string>) {
+    return terms.debounceTime(400)
+      .distinctUntilChanged()
+      .switchMap(term => this.searchEntries(term));
+  }
+
+  searchEntries(term) {
+    return this.http
+        .get(this.baseUrl + this.queryUrl + term)
+        .map(res => res.json());
+  }
+
 }
